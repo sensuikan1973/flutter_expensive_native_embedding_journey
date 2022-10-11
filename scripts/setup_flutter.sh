7410155591a5c395833c7e75dfdff454bcd25641
sensuikan1973 <sensuikan1973@gmail.com>
2022-10-12 03:44:49 +0900

    add script to setup flutter

---
+++ scripts/setup_flutter.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env zsh
+
+set -euxo pipefail
+
+flutter channel master
+
+flutter config --no-analytics
+
+flutter config --enable-ios
+flutter config --enable-android
+flutter config --enable-macos-desktop
+
+flutter config --no-enable-windows-desktop
+flutter config --no-enable-linux-desktop
+flutter config --no-enable-web
+
+flutter clean
+flutter upgrade
