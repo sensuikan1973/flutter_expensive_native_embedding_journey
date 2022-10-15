0173a34872b308e5c05b7d9e33d72f937f85c577
sensuikan1973 <sensuikan1973@gmail.com>
2022-10-15 18:10:07 +0900

    edit

--- slide/global-bottom.vue
+++ slide/global-bottom.vue
@@ -1,9 +1,9 @@
 <!-- https://github.com/slidevjs/slidev/issues/64#issuecomment-838084965 -->
 <template>
-  <!-- END page で "(N + 1) / N" というページ表記になるのを回避するために、hasNext を参照している -->
+  <!-- END page で "(N + 1) / N" というページ表記になるのを回避するために条件分岐している -->
   <!-- https://sli.dev/custom/vue-context.html#slidev-nav -->
   <!-- https://github.com/slidevjs/slidev/blob/main/packages/client/logic/nav.ts -->
-  <footer v-if="$slidev.nav.hasNext" class="absolute bottom-0 right-0 p-2">
+  <footer v-if="$slidev.nav.currentPage <= $slidev.nav.total" class="absolute bottom-0 right-0 p-2">
     <SlideCurrentNo /> / <SlidesTotal />
   </footer>
 </template>
