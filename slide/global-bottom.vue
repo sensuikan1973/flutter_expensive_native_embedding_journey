ce1479fbc899a5b6588c7c3eee6d7833d16fc24a
sensuikan1973 <sensuikan1973@gmail.com>
2022-10-15 15:18:05 +0900

    add comment

--- slide/global-bottom.vue
+++ slide/global-bottom.vue
@@ -1,6 +1,8 @@
 <!-- https://github.com/slidevjs/slidev/issues/64#issuecomment-838084965 -->
 <template>
   <!-- END page で "(N + 1) / N" というページ表記になるのを回避するために、hasNext を参照している -->
+  <!-- https://sli.dev/custom/vue-context.html#slidev-nav -->
+  <!-- https://github.com/slidevjs/slidev/blob/main/packages/client/logic/nav.ts -->
   <footer v-if="$slidev.nav.hasNext" class="absolute bottom-0 right-0 p-2">
     <SlideCurrentNo /> / <SlidesTotal />
   </footer>
