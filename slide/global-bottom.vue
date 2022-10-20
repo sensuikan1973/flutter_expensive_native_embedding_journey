b37267a3aabbb1a5d797ff2e8d3c30e9ea980dc4
sensuikan1973 <sensuikan1973@gmail.com>
2022-10-21 04:49:08 +0900

    edit

--- slide/global-bottom.vue
+++ slide/global-bottom.vue
@@ -3,7 +3,7 @@
   <!-- END page で "(N + 1) / N" というページ表記になるのを回避するために条件分岐している -->
   <!-- https://sli.dev/custom/vue-context.html#slidev-nav -->
   <!-- https://github.com/slidevjs/slidev/blob/main/packages/client/logic/nav.ts -->
-  <footer v-if="$slidev.nav.currentPage <= $slidev.nav.total" class="absolute bottom-0 right-0 p-2">
+  <footer v-if="$slidev.nav.currentPage <= $slidev.nav.total" class="absolute bottom-0 right-0 p-1">
     <SlideCurrentNo /> / <SlidesTotal />
   </footer>
 </template>
